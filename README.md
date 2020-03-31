3672e65837dd34e2271c8212e531bfdd219d8867
Kishore.Kameshwar <kishore.kameshwar@experitest.com>
2020-03-31 15:19:22 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Mobile Automation basics using Java/Appium
+
+This project demonstrates Mobile Automation using Java and Appium in IntelliJ IDE.
+Project is associated with the Blog  # Complete Guide to Android Automation at link - <>
+
+This example will cover:
+
+Basic Test case creation using Java/TestNG framework using Appium Library/Appium Server.
+
+### Getting Started
+
+1. Clone this git repository
+
+	```
+	git clone
+	```
+
+2. Start Appium Server using Appium Desktop installed in your PC.
+   The project expects the Appium Server to run on localhost:4723. If you run the server to different host and port. Please change the code.
+
+3. Download the Eribank application using URL : https://experitest.s3.amazonaws.com/eribank.apk to c:\\  (The code uses the Application from c:\\ (in windows). Please change the code in case you   change the download location)
+
+4. Modify following variables in the code if necessary
+   Open EribankTest.java and modify static variables if necessary,
+    Appium Server listening host and port
+      a) public static final String APPIUM_SRV_URL = "http://localhost:4723/wd/hub";
+    Path of the Eribank application (in case downloaded location is different)
+      b) public static final String APP_PATH = "C:\\eribank.apk";
+    Device name (After excecuting adb devices. See "Android device recognition" in the blogs)
+      c) public static final String DEVICE_NAME = "a3ae1c63";
+5. Please follow the section "Executing the Test" in the blog.
---
+++ src/main/java/test/EribankTest.java
@@ -0,0 +1,59 @@
+package test;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.By;
+import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Class used to automate Eribank app using Java/Appium.
+ *
+ *
+ */
+public class EribankTest {
+
+    DesiredCapabilities dc = new DesiredCapabilities();
+    protected AndroidDriver<AndroidElement> driver = null;
+
+    public static final String APPIUM_SRV_URL = "http://localhost:4723/wd/hub";
+    public static final String APP_PATH = "C:\\eribank.apk";
+    public static final String DEVICE_NAME = "a3ae1c63";
+
+    @BeforeTest
+    public void setUp() throws MalformedURLException {
+
+        dc.setCapability(MobileCapabilityType.APP, APP_PATH);
+        dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.experitest.ExperiBank");
+        dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".LoginActivity");
+        dc.setCapability("platformName","Android");
+        dc.setCapability("deviceName", DEVICE_NAME);
+
+        driver = new AndroidDriver<>(new URL(APPIUM_SRV_URL), dc);
+    }
+
+    @Test
+    public void testEribankLogin() {
+        By updateOKBy = By.xpath("//*[@text='OK']");
+        if (driver.findElements(updateOKBy).size() > 0) {
+            driver.findElement(By.xpath("//*[@text='OK']")).click();
+        }
+        By userElemBy = By.xpath("//*[@text='Username']");
+        driver.findElement(userElemBy).sendKeys("company");
+        driver.findElement(By.xpath("//*[@text='Password']")).sendKeys("company");
+        driver.findElement(By.xpath("//*[@text='Login']")).click();
+
+    }
+}
